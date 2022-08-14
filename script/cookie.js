ac556c4e27fd9d33bb74e94209b834f2040a668b
Wakidzashii <91572053+Wakidzashii@users.noreply.github.com>
2022-08-14 16:43:28 +0300

    Delete script directory

--- script/cookie.js
+++
@@ -1,7 +0,0 @@
-const cookies = document.querySelector('.notification_btn')
-const cookiesdrop = document.querySelector('.cookies-wrapper')
-
-cookies.addEventListener('click', function(){
-   
-    cookiesdrop.classList.remove('active')
-});
